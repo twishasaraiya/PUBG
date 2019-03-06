4f6ceb6a45b8a8e4d60efce825b632f64cfde1d8
twishasaraiya <saraiyatwisha@gmail.com>
2019-03-06 22:25:45 +0530

    update API

--- api.py
+++ api.py
@@ -16,9 +16,11 @@ BASE_URL = 'https://api.pubg.com/shards/';
 
 ## Your pubg api key here
 PUBG_API_KEY = config.api_key
-
+## Your pubg model here
 FILE_NAME = "model/lgb.pkl"
 
+PLAYER_ID = 'jmjkh'
+PLATFORM_NAME = ''
 ##################### HANDLE 404 ERROR #############################
 @app.errorhandler(404)
 @cross_origin()
@@ -36,9 +38,11 @@ def not_found(error=None):
 
 ##################### GET MATCHID FOR GIVEN PLAYER ##################
 
-def getMatchIdOfPlayer(platform_name,player_name):
+def getMatchIdOfPlayer(player_name):
     # call the pubgAPI to get player info
-    url = BASE_URL + platform_name + '/players?filter[playerNames]=' + player_name
+    global PLAYER_ID
+    print(' ######################## get match id of player ######################## ')
+    url = BASE_URL + PLATFORM_NAME + '/players?filter[playerNames]=' + player_name
     print('url= {}'.format(url))
     headers = {
         "Authorization" : PUBG_API_KEY,
@@ -48,39 +52,49 @@ def getMatchIdOfPlayer(platform_name,player_name):
     r = R.get(url,headers=headers)
 
     if r.status_code == 200:
+        print('data returned from pubg api')
         r = json.loads(r.text)
         data = r["data"][0]
         #print('id=',data['id'])
-        player_id = data['id']
+        PLAYER_ID = data['id']
+        print('storing player id ',PLAYER_ID)
         matches = data['relationships']['matches']['data']
         # this will set the match_id as last id of the last match
-        for match in matches:
-            match_id = match['id']
-        return (player_id,match_id)
+        return matches
+        #for match in matches:
+        #    match_id = match['id']
+        #return (player_id,match_id)
     else:
         return None
 
+##################### GET PLAYER LIFETIME STATS ##################
+def lifetimeStats():
+    
 
 ##################### GET MATCH INFO FOR GIVEN MATCHID ##################
 
-def getMatchInfoForPlayer(platform_name,match_id,player_id):
-    url = BASE_URL + platform_name + '/matches/' + match_id
+def getMatchInfoForPlayer(match_id):
+    print(' ######################## get match info for player #################')
+    url = BASE_URL + PLATFORM_NAME + '/matches/' + match_id
     print('get match info url = {}'.format(url))
     headers ={
         "Authorization" : PUBG_API_KEY,
         "Accept" : "application/vnd.api+json",
         "Accept-Encoding":"gzip"
     }
-    r= R.get(url,headers=headers)
+    match_info = {}
 
+    r= R.get(url,headers=headers)
+    print(r.status_code)
     if r.status_code == 200:
         r = json.loads(r.text)
-        match_type = r["data"]["attributes"]["gameMode"]
+        match_info['attributes'] = r["data"]["attributes"]
         data = r["included"]
         for obj in data:
-            if (obj["type"] == "participant") and (obj["attributes"]["stats"]["playerId"] == player_id):
-                match_info = obj["attributes"]["stats"]
-                return match_info, match_type
+            if (obj["type"] == "participant"):
+                if (obj["attributes"]["stats"]["playerId"] == PLAYER_ID):
+                    match_info['stats'] = obj["attributes"]["stats"]
+                    return match_info
     else:
         return None
 
@@ -89,10 +103,10 @@ def getMatchInfoForPlayer(platform_name,match_id,player_id):
 def data_processing(match_info,match_type):
     drop_features = ['deathType', 'killPointsDelta', 'lastKillPoints', 'lastWinPoints', 'mostDamage', 'name', 'playerId', 'timeSurvived', 'winPointsDelta', 'winPlace']
 
-    final_pos = match_info['winPlace']
+    final_pos = match_info['stats']['winPlace']
 
     #create a copy to modify
-    stats =  match_info
+    stats =  match_info['stats']
     # reomove extra keys in match_info
     for x in drop_features:
         if x in stats:
@@ -112,11 +126,14 @@ def predict(stats):
     print('Model Columns Loaded')
     ## generate X as dataframe from stats from PUBG api
     stats_df = pd.get_dummies(pd.DataFrame(stats,index=[0]))
-    X = stats_df.reindex(columns = model_columns, fill_value = 0)
+    X = stats_df.reindex(columns = model_columns)
+    print(X.columns)
     X.drop(['Id','matchId','groupId','matchType','winPlacePerc'],axis = 1, inplace=True)
     preds = model.predict(X)
-    print('WINNING PROBABILITY:',preds)
-    return preds
+    print('WINNING PROBABILITY:',preds,type(preds))
+    if(preds>1):
+         return 1
+    return preds.astype(int)
 
 
 @app.route("/",methods=['GET'])
@@ -125,36 +142,52 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/",methods=['POST'])
+@app.route("/predict",methods=['POST','OPTIONS'])
+@cross_origin()
+def prediction():
+    print('################ STARTING PREDICTION ###################')
+    print('The request \n method: {} \n Type JSON: {} \n Content-Type: {}'.format(request.method,request.is_json,request.headers['Content-Type']))
+
+    resp = {}
+
+    if request.method == 'POST':
+        match_id = request.get_json()
+        print('HERE',PLAYER_ID,match_id)
+        #get match information from match_id for player_id
+        if match_id is not None and PLAYER_ID is not None:
+            match_info = getMatchInfoForPlayer(match_id)
+            print('returned match info',match_info)
+            #print('match info=',match_info)
+            match_type = match_info['attributes']['gameMode']
+            ## performing pre-processing on match_info data
+            stats = data_processing(match_info,match_type)
+            #  TODO: use this match info to predict the outcome
+            output = predict(stats)
+            print(output,type(output))
+            output = np.round(output,decimals=4)*100
+            resp['matchInfo'] = match_info['attributes']
+            resp['stats'] = stats
+            print(output)
+            resp['output'] = output.item()
+            return jsonify(resp)
+    else:
+        return not_found()
+
+
+@app.route("/",methods=['POST','OPTIONS'])
 @cross_origin()
 def api():
-    print('The request \n method: {} \n Type JSON: {} \n Content-Type: {}'.format(request.method,request.is_json,request.headers['Content-Type']))
+    global PLATFORM_NAME
+    #print('The request \n method: {} \n Type JSON: {} \n Content-Type: {}'.format(request.method,request.is_json,request.headers['Content-Type']))
     if request.method == 'POST':
-        print('HERE',request.form)
-        data = request.form
-        platform_name = 'xbox'
-        player_name = data['player-name']
-        print(platform_name,player_name)
-        # get match id of the given player
-        player_id, match_id = getMatchIdOfPlayer(platform_name,player_name)
-        print(player_id,match_id)
-
-        #get match information from match_id for player_id
-        if match_id is not None and player_id is not None:
-            match_info, match_type = getMatchInfoForPlayer(platform_name,match_id,player_id)
-            #print('match info=',match_info)
-
-        ## performing pre-processing on match_info data
-        stats = data_processing(match_info,match_type)
-        #  TODO: use this match info to predict the outcome
-        output = predict(stats)
-        print('Returning Response')
-        #resp = jsonify(winning_prob = output[0])
-        #resp.headers['Content-Type'] = 'application/json'
-        #resp.status_code = 200
-
-        output = np.round(output,decimals=4)*100
-        return render_template('index.html',stats= match_info, prediction=output[0])
+        json = request.get_json()
+        PLATFORM_NAME = json['platform-name']
+        player_name = json['player-name']
+        print(PLATFORM_NAME,player_name)
+        # get all matches id of the given player
+        matches = getMatchIdOfPlayer(player_name)
+        print('MATCHES => ',matches)
+        return jsonify(matches)
     else:
         return not_found()
 
